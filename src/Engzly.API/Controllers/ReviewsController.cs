689a69647c29cf1d44d94ae52c23fb0d310fe372
Youssef Elkomy <youssefelkomy500@gmail.com>
2026-03-03 02:02:13 +0200

    feat(reputation): add ReviewsController (#6)

---
+++ src/Engzly.API/Controllers/ReviewsController.cs
@@ -0,0 +1,17 @@
+﻿using Engzly.Application.Features.Reviews.Commands.PostReview;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Engzly.API.Controllers;
+
+public sealed class ReviewsController(ISender mediator) : BaseApiController
+{
+    // [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<string>> Create([FromBody] PostReviewCommand command)
+    {
+        var response = await mediator.Send(command);
+
+        return Ok(new { ReviewId = response });
+    }
+}
