7ef1edc8feb57c0530ffe33e4a196f3773706642
Youssef Mohamed <mohmmeadkhalef22@gmail.com>
2026-06-20 19:16:14 +0300

    Fix : Adding Amount Properties in CreatePaymentCommand Class

--- src/Engzly.Application/Features/Payments/Commands/Models/CreatePaymentCommand.cs
+++ src/Engzly.Application/Features/Payments/Commands/Models/CreatePaymentCommand.cs
@@ -6,5 +6,6 @@ namespace Engzly.Application.Features.Payments.Commands.Models
     public sealed class CreatePaymentCommand : IRequest<Response<string>>
     {
         public string GigId { get; set; } = null!;
+        public decimal Amount { get; set; }
     }
 }
